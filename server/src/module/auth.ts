1fe8eaf3d92548287ca99c020dbc7fff86f5a826
sabrina tamda <sabrinatamda5@gmail.com>
2024-04-23 15:35:46 +0200

    add dotenv config

--- server/src/module/auth.ts
+++ server/src/module/auth.ts
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 import * as bcrypt from "bcrypt";
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 export type JWTUser = {
     id: string;
     username: string;
