20eb74256d64029bc81da87619bc67e91d98b373
Sara Bevilacqua <sarabev6@gmail.com>
2024-05-29 20:17:59 +0200

    fix: remove my articles page list

--- client/src/components/Article/ArticleList.tsx
+++ client/src/components/Article/ArticleList.tsx
@@ -1,8 +1,10 @@
-import ArticleCard from "../Content/ArticleCard";
+import ArticleCard from "./Content/ArticleCard";
+import MyArticleCard from "./Content/MyArticleCard";
 
 const ArticleList = (): JSX.Element => {
   return (
     <>
+      <MyArticleCard />
       <ArticleCard />
       <ArticleCard />
     </>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import { Container, Heading, Text, Box } from "@chakra-ui/react";
-import ArticleList from "../components/Article/Lists/ArticleList";
+import ArticleList from "../components/Article/ArticleList";
 import { blue_color } from "../assets/customColors";
 import SelectArticles from "../components/Article/Actions/SelectArticles";
 
