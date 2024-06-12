0529cff4308a6d06f8052716ed589417c6f05ca6
sabrina tamda <sabrinatamda5@gmail.com>
2024-06-12 20:32:22 +0200

    update resolver

--- server/src/resolver.ts
+++ server/src/resolver.ts
@@ -4,12 +4,12 @@ import {signIn} from "./mutation/user/signin.js";
 import {deleteUser} from "./mutation/user/delete-user.js";
 import {createArticle} from "./mutation/article/create-article.js";
 import {updateArticle} from "./mutation/article/update-article.js";
-import {getArticles} from "./mutation/article/get-articles.js";
 import {incrementOrDecrementLikes} from "./mutation/article/like/increment-or-decrement-like.js";
 import {deleteArticle} from "./mutation/article/delete-article.js";
 import {addComment} from "./mutation/article/comment/add-comment.js";
 import { deleteComment } from "./mutation/article/comment/delete-comment.js";
-import {getArticle} from "./mutation/article/get-article.js";
+import { getArticles } from "./queries/get-articles.js";
+import { getArticle } from "./queries/get-article.js";
 
 
 
